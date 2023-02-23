83ca475fa301f48cd63955fe5de3305080ce6596
Benjamin <benj.james.carter@gmail.com>
2023-02-22 17:39:37 -0800

    expanded TODO

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ TODO: handle duplicate job postings
 TODO: add error handling for 404 errors or other exceptions
 TODO: explore rate limits, for scraper
 TODO: optimize for the given rate limits
-TODO: configure scrape to run against all available jobs for a given search, and retain the number of jobs scraped. 
+TODO: configure scrape to run against all available jobs for a given search, and retain the number of jobs scraped.
+TODO: perform term search and export to CSV file
 """
 
 
-url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Software%2BEngineer&location=Seattle%2C%2BWashington%2C%2BUnited%2BStates&locationId=&geoId=104116203&sortBy=R&f_TPR=r604800&distance=50&start="
+url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Software%2BEngineer&location=Seattle%2C%2BWashington%2C%2BUnited%2BStates&locationId=&geoId=104116203&sortBy=R&f_TPR=r604800&distance=50&start=" #0 or 25 
 
 
 def linkedin_scrape(url, page_num):
@@ -33,7 +34,7 @@ def linkedin_scrape(url, page_num):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # if we have not already seen this job, scrape its data.
-        if listing not in scraped_jobs_list:
+        if job_url not in scraped_jobs_list:
             # gets the job post body text
             body = soup.find('div', class_="show-more-less-html__markup").text
 
@@ -47,7 +48,7 @@ def linkedin_scrape(url, page_num):
 
             # adds the job_url to the scraped jobs list, after the scrape has been completed
             scraped_jobs_list.append(job_url)
-            print(scraped_jobs_list)
+            print(job_url)
         else:
             # if we hit this else, we have already scraped this job, and should proceed to the next in the for loop.
             continue
