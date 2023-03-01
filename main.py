bf7bfac2d792ce1549954f66e35b209e7f973b95
Benjamin <benj.james.carter@gmail.com>
2023-02-28 20:23:44 -0800

    testing and iterating

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -13,11 +13,11 @@ TODO: add error handling for 404 errors or other exceptions
 TODO: explore rate limits, for scraper
 TODO: optimize for the given rate limits
 TODO: configure scrape to run against all available jobs for a given search, and retain the number of jobs scraped.
-TODO: perform term search and export to CSV file
+TODO: perform term search and export Json
 """
 
 
-url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Software%2BEngineer&location=Seattle%2C%2BWashington%2C%2BUnited%2BStates&locationId=&geoId=104116203&sortBy=R&f_TPR=r604800&distance=50&start=" #0 or 25 
+url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Software%2BEngineer&location=Seattle%2C%2BWashington%2C%2BUnited%2BStates&locationId=&geoId=104116203&sortBy=R&f_TPR=r604800&distance=50&start="  # 0 or 25
 
 
 def linkedin_scrape(url, page_num):
@@ -41,6 +41,7 @@ def linkedin_scrape(url, page_num):
             # gets the job post body text
             body = soup.find('div', class_="show-more-less-html__markup").text
 
+            # a is append, W will write a new file each time
             with open('bulk_search_text.txt', 'a') as f:
                 f.write('\n')
                 f.write(str(job_url))  # writes the job URL
@@ -57,7 +58,7 @@ def linkedin_scrape(url, page_num):
             continue
 
 # uncomment the line below and run it to perform a initial scrape and create a raw text file of job information.
-# linkedin_scrape(url, 0) 
+# linkedin_scrape(url, 0)
 
 
 # TODO: scrape jobs from indeed
@@ -67,50 +68,162 @@ TODO: create list of regex patterns
 TODO: import patterns as list
 TODO: search through bulk text data, for each regex pattern /gmi global multiline insensitive search
 TODO: save results as dictionary key value pairs
-TODO: output the results to a Json Key value object 
+TODO: output the results to a Json Key value object
 """
-#importing file-todo: import file!
-def regex_terms(regex_file):
-    """_summary_
-    Args:
-        regex_file (.txt): .txt file containing tech terms, and matching regex patterns
-    Returns:
-        Dict: a dictionary of key value pairs where key is the term and value is the regex pattern
+# importing file-todo: import file!
 
-    NOTE: I believe that this is working as intended and the issue is limited to printing the regex patterns. 
 
-    In this case, we must procced with the application (beginning with the find_tech_occurances function) until we output something that shows us if the regex patterns are working.
-    """
-    with open(regex_file, "r") as f:
-        regex_list = f.readlines()
-        # Laravel: \bLaravel\b {'Laravel': '\bLaravel\b', 'PHP': '\bPHP\b'}
-    patterns_dict = {}
-    for term in regex_list:
-        try:
-            key, value = term.strip().split(": ")
-            patterns_dict[key] = value
+# def regex_terms(regex_file):
+#     """_summary_
+#     Args:
+#         regex_file (.txt): .txt file containing tech terms, and matching regex patterns
+#     Returns:
+#         Dict: a dictionary of key value pairs where key is the term and value is the regex pattern
 
-        except ValueError:
-            continue
+#     NOTE: I believe that this is working as intended and the issue is limited to printing the regex patterns.
 
-    return patterns_dict
+#     In this case, we must procced with the application (beginning with the find_tech_occurances function) until we output something that shows us if the regex patterns are working.
+#     """
+#     with open(regex_file, "r") as f:
+#         regex_list = f.readlines()
+#         # Laravel: \bLaravel\b {'Laravel': '\bLaravel\b', 'PHP': '\bPHP\b'}
+#     patterns_dict = {}
+#     for term in regex_list:
+#         try:
+#             key, value = term.strip().split(": ")
+#             patterns_dict[key] = value
 
-path = 'patterns.txt'
-regex_dict = regex_terms(path)
-print(regex_dict)
+#         except ValueError:
+#             continue
+
+#     return patterns_dict
+
+
+patterns_path = 'patterns.txt'
+
 
 
 # from patterns import patterns_dict
 # print(patterns_dict)
+def create_tech_list(path):
+    with open(path, 'rt') as f:
+        text_content = f.read()
+    return text_content
+
+
+techs = create_tech_list(patterns_path)
+techs = techs.split("\n")
+
+def open_text_file(text_file):
+    """
+    Function opens a text file, and returns the contents as a string.
+    :param text_file: a .txt file to open
+    :return: contents of the file converted into a string
+    """
+    with open(text_file, 'rt') as f:
+        text_content = f.read()
+        # print(text_content)
+    return text_content
+
 
 def find_tech_occurrences(text_file, patterns_dict):
     """
-    TODO: open the raw text file
+    DONE: open the raw text file
     TODO: for each value in patterns_dict, search through the raw text
     TODO: incriment a new results dict, where key = key from patterns_dict, and value = number of times that term was found in raw text
     TODO: return results dict
     """
-    pass
+    counts_dict = {}  # where key is tech, and value is number of times found in text file
+
+    # get the raw text into a string format # step one done
+    text_string = open_text_file(text_file)
+    # a list of each job that was found
+    jobs_list = text_string.split(
+        "----------------------------------------------------------------")
+    counts_list = []
+    jobs_count = {}
+    count = 0
+    for job in jobs_list:
+        if count > 2:
+            for pattern in patterns_dict.values():
+                # r"\.NET\sCORE\b"mi < re_pattern should be formatted to match exactly this for each pattern
+                # r"\bAWS\sLambda\b"mi <
+                re_pattern = pattern
+                print(re_pattern)
+                x = re.search(pattern, job)
+                #  ['.Net Core', '.net core'] len = 2 , ['AWS Lambda'] len = 1
+                print(x)
+                # for word in text_string:
+                #     matches = re.findall(word, re_pattern)
+                #     print(matches)
+        count += 1
+
+
+""" Regex-less match counting method
+for word in words_list:( or words dict.)
+    for job in jobs_list:
+        if word in job:
+            CHECK EITHER SIDE OF WORD
+                if blank space on either side of word:
+                    We have a positive, incriment the count.
+                else (not whitespace next to the word)
+                    move on to next word job post.
+"""
+text = open_text_file('bulk_search_text.txt')
+
+    # a list of each job that was found
+jobs_list = text.split(
+    "----------------------------------------------------------------")
+
+def has_whitespace_around_word(word, string):
+    """
+    Check if there is whitespace around a given word within a larger string.
+    """
+    pattern = r'\b{}\b[,.]?'.format(re.escape(word))
+    match = re.search(pattern, string)
+    if match:
+        start, end = match.span()
+        if start > 0 and string[start-1].isspace() or \
+            end < len(string) and string[end].isspace():
+            return True
+    return False
+
+
+def find_techs_regexless(text_file, techs_list):
+    word_search_dict = {} #initialize dictionary 
+    # get the raw text into a string format # step one done
+    jobs_list = text_file.split(
+        "----------------------------------------------------------------")
+    print(techs_list)
+    count = 0
+    for job in jobs_list:  # for each individual job we scrape > job 2
+        job = job.upper() 
+        count += 1
+        print(f"on job #:{count}")
+        for word in techs_list:  # for each tech we are looking for Job 2
+            word = word.upper()
+            if word in job: 
+                result = has_whitespace_around_word(word, job)
+                if result:
+                    #print("inside if result truthy check")
+                    if word in word_search_dict:
+                        word_search_dict[word] += 1# add word to a new dict, 
+                    else:
+                        word_search_dict[word] = 1#increment value by += 1
+                        print(word_search_dict)
+            else:
+                print("hit else condition, if word in job > false")
+                continue
+    return word_search_dict
+
+find_techs_regexless(text, techs)
+
+# ['NET CORE,', 'APS.NET,', 'AWS Elastic Load Balancing,', 'AWS Lambda,', 'C++,', 'Java,', 'Angular']
+
+
+bulk_text = 'bulk_search_text.txt'
+# find_tech_occurrences(bulk_text, regex_dict)
+
 
 def results_to_json(results_dict):
     """
@@ -118,11 +231,13 @@ def results_to_json(results_dict):
     """
     pass
 
+
 def upload_results_to_db(results_json):
     """
     TODO: sort out all the DB stuff.....
     """
     pass
+
 
 """
 steps
@@ -134,4 +249,3 @@ steps
 * convert that dictionary to .JSON format
 
 """
-
---
+++ temp.py
@@ -0,0 +1,345 @@
+
+#     'Amazon CloudFront': r'\bAmazon\sCloudFront\b',
+#     'Amazon CloudWatch': r'\bAmazon\sCloudWatch\b',
+#     'Amazon EC2': r'\bAmazon\sEC2\b',
+#     'Amazon EC2 Container Service': r'\bAmazon\sEC2\sContainer\sService\b',
+#     'Amazon RDS': r'\bAmazon\sRDS\b',
+#     'Amazon Route 53': r'\bAmazon\sRoute\s53\b',
+#     'Amazon S3': r'\bAmazon\sS3\b',
+#     'Amazon SES': r'\bAmazon\sSES\b',
+#     'Android SDK': r'\bAndroid\sSDK\b',
+#     'Android Studio': r'\bAndroid\sStudio\b',
+#     'Angular': r'\bAngular\b',
+#     'Animate.css': r'\bAnimate.css\b',
+#     'AnimeJS': r'\bAnimeJS\b',
+#     'Ansible': r'\bAnsible\b',
+#     'Apache HTTP Server': r'\bApache\sHTTP\sServer\b',
+#     'Apache Tomcat': r'\bApache\sTomcat\b',
+#     'Asana': r'\bAsana\b',
+#     'Atom': r'\bAtom\b',
+#     'Babel': r'\bBabel\b',
+#     'Backbone.js': r'\bBackbone.js\b',
+#     'Bitbucket': r'\bBitbucket\b',
+#     'BootStrap': r'\bBootStrap\b',
+#     'Bower': r'\bBower\b',
+#     'C#': r'\bC#\b',
+#     'CSS3': r'\bCSS\s3\b',
+#     'Cassandra': r'\bCassandra\b',
+#     'CentOS': r'\bCentOS\b',
+#     'CircleCI': r'\bCircleCI\b',
+#     'Chart.JS': r'\bChart.JS\b',
+#     'CloudFlare': r'\bCloudFlare\b',
+#     'Confluence': r'\bConfluence\b',
+#     'D3': r'\bD3\b',
+#     'Datadog': r'\bDatadog\b',
+#     'Debian': r'\bDebian\b',
+#     'DigitalOcean': r'\bDigitalOcean\b',
+#     'Django': r'\bDjango\b',
+#     'Docker': r'\bDocker\b',
+#     'Docker Compose': r'\bDocker\sCompose\b',
+#     'ES6': r'\bES6\b',
+#     'ESLint': r'\bESLint\b',
+#     'Elasticsearch': r'\bElasticsearch\b',
+#     'Electron': r'\bElectron\b',
+#     'ExpressJS': r'\bExpressJS?\b',  # special regex needed
+#     'Firebase': r'\bFirebase\b',
+#     'Flask': r'\bFlask\b',
+#     'Flutter': r'\bFlutter\b',
+#     'FullPageJS': r'\bFullPageJS?\b',  # special regex needed
+#     'G Suite': r'\bG\sSuite\b',
+#     'Git': r'\bGit\b',
+#     'GitHub': r'\bGitHub\b',
+#     'GitHub Pages': r'\bGitHub\sPages\b',
+#     'GitLab': r'\bGitLab\b',
+#     'Glimmer': r'\bGlimmer\b',
+#     'Google AdSense': r'\bGoogle\sAdSense\b',
+#     'Google Analytics': r'\bGoogle\sAnalytics\b',
+#     'Google App Engine': r'\bGoogle\sApp\sEngine\b',
+#     'Google Cloud Platform': r'\bGoogle\sCloud\sPlatform\b',
+#     'Google Compute Engine': r'\bGoogle\sCompute\sEngine\b',
+#     'Google Tag Manager': r'\bGoogle\sTag\sManager\b',
+#     'Gradle': r'\bGradle\b',
+#     'Grafana': r'\bGrafana\b',
+#     'GrapQL': r'\bGraphQL\b',
+#     'Groovy': r'\bGroovy\b',
+#     'Grunt': r'\bGrunt\b',
+#     'HTML5': r'\bHTML5\b',
+#     'Hadoop': r'\bHadoop\b',
+#     'Handlebars': r'\bHandlebars.js\b',
+#     'Heroku': r'\bHeroku\b',
+#     'HubSpot': r'\bHubSpot\b',
+#     'InVision': r'\bInVision\b',
+#     'IntelliJ IDEA': r'\bIntelliJ\sIDEA\b',
+#     'Intercom': r'\bIntercom\b',
+#     'Ionic': r' \bIonic\b',
+#     'Java': r'\bJava\b',
+#     'JavaScript': r'\bJavaScript\b',
+#     'Jenkins': r'\bJenkins\b',
+#     'Jira': r'\bJira\b',
+#     'Kafka': r'\bKafka\b',
+#     'Kibana': r'\bKibana\b',
+#     'Kotlin': r'\bKotlin\b',
+#     'Kebernetes': r'\bKubernetes\b',
+#     'Laravel': r'\bLaravel\b',
+#     'Leaflet': r'\bLeaflet\b',
+#     'Lodash': r'\bLodash\b',
+#     'Logstash': r'\bLogstash\b',
+#     'Mailgun': r'\bMailgun\b',
+#     'MariaDB': r'\bMariaDB\b',
+#     'Markdown': r'\bMarkdown\b',
+#     'Matlab': r'\bMatLab\b',
+#     'Material Design for Angular': r'\bMaterial\sDesign\sfor\sAngular\b',
+#     'Memchached': r'\bMemcached\b',
+#     'Microsoft Azure': r'\bMicrosoft\sAzure\b',
+#     'Microsoft IIS': r'\bMicrosoft\sIIS\b',
+#     'Microsoft SQL Server': r'\bMicrosoft\sSQL\sServer\b',
+#     'Mixpanel': r'\bMixpanel\b',
+#     'Modernizr': r'\bModernizr\b',
+#     'MongoDB': r'\bMongoDB\b',
+#     'MySQL': r'\bMySQL\b',
+#     'NGINX': r'\bNGINX\b',
+#     'New Relic': r'\bNew\sRelic\b',
+#     'Next.JS': r'\bNext.JS\b',
+#     'NoSQL': r'\bNoSQL\b',
+#     'Node.js': r'\bNode.js\b',  # special regex needed
+#     'Notepad++': r'\bNotepad\+\+\b',  # special regex needed
+#     'Objective-C': r'\bObjective-C\b',
+#     'OpenSSL': r'\bOpenSSL\b',
+#     'Oracle': r'\bOracle\b',
+#     'PHP': r'\bPHP\b',
+#     'PayPal': r'\bPayPal\b',
+#     'Perl': r'\bPerl\b',
+#     'PhpStorm': r'\bPhpStorm\b',
+#     'PostgreSQL': r'\bPostgreSQL\b',
+#     'Postman': r'\bPostman\b',
+#     'Polymer': r'\bPolymer\b',
+#     'PyCharm': r'\bPyCharm\b',
+#     'Python': r'\bPython\b',
+#     'RabbitMQ': r'\bRabbitMQ\b',
+#     'Rails': r'\bRails\b',
+#     'React': r'\bReact\b',
+#     'React Native': r'\bReact\sNative\b',
+#     'Redis': r'\bRedis\b',
+#     'Redux': r'\bRedux\b',
+#     'RequireJS': r'\bRequireJS\b',
+#     'Ruby': r'\bRuby\b',
+#     'SQL': r'\bSQL\b',
+#     'SQLite': r'\bSQLite\b',
+#     'Sass': r'\bSass\b',
+#     'Scala': r'\bScala\b',
+#     'Selenium': r'\bSelenium\b',
+#     'Sentry': r'\bSentry\b',
+#     'Shopify': r'\bShopify\b',
+#     'Skype': r'\bSkype\b',
+#     'Slack': r'\bSlack\b',
+#     'Slick': r'\bSlick\b',
+#     'Socket.IO': r'\bSocket.IO\b',  # special regex needed
+#     'SourceTree': r'\bSourceTree\b',
+#     'Spring Boot': r'\bSpring\sBoot\b',
+#     'Stack Overflow': r'\bStack\sOverflow\b',
+#     'Stripe': r'\bStripe\b',
+#     'Sublime Text': r'\bSublime\sText\b',
+#     'Swift': r'\bSwift\b',
+#     'Symfony': r'\bSymfony\b',
+#     'TensorFlow': r'\bTensorFlow\b',
+#     'Terraform': r'\bTerraform\b',
+#     'Travis CI': r'\bTravis\sCI\b',
+#     'Trello': r'\bTrello\b',
+#     'Twilio': r'\bTwilio\b',
+#     'Twilio SendGrid': r'\bTwilio\sSendGrid\b',
+#     'TypeScript': r'\bTypeScript\b',
+#     'Ubuntu': r'\bUbuntu\b',
+#     'Vagrant': r'\bVagrant\b',
+#     'Varnish': r'\bVarnish\b',
+#     'Vim': r'\bVim\b',
+#     'VirtualBox': r'\bVirtualBox\b',
+#     'Visual Studio': r'\bVisual\sStudio\b',
+#     'Visual Studio Code': r'\bVisual\sStudio\sCode\b',
+#     'Vue.js': r'\bVue\.js\b',  # special regex needed
+#     'Webpack': r'\bWebpack\b',
+#     'WebStorm': r'\bWebStorm\b',
+#     'WooCommerce': r'\bWooCommerce\b',
+#     'WordPress': r'\bWordPress\b',
+#     'Xamarin': r'\bXamarin\b',
+#     'Xcode': r'\bXcode\b',
+#     'Yarn': r'\bYarn\b',
+#     'Zendesk': r'\bZendesk\b',
+#     'gulp': r'\bgulp\b',
+#     'jQuery': r'\bjQuery\b',
+#     'jQuery UI': r'\bjQuery\sUI\b',
+#     'npm': r'\bnpm\b'
+# }
+
+
+
+
+
+Amazon CloudFront: \bAmazon\sCloudFront\b
+Amazon CloudWatch: \bAmazon\sCloudWatch\b
+Amazon EC2: \bAmazon\sEC2\b
+Amazon EC2 Container Service: \bAmazon\sEC2\sContainer\sService\b
+Amazon RDS: \bAmazon\sRDS\b
+Amazon Route 53: \bAmazon\sRoute\s53\b
+Amazon S3: \bAmazon\sS3\b
+Amazon SES: \bAmazon\sSES\b
+Android SDK: \bAndroid\sSDK\b
+Android Studio: \bAndroid\sStudio\b
+Angular: \bAngular\b
+Animate.css: \bAnimate.css\b
+AnimeJS: \bAnimeJS\b
+Ansible: \bAnsible\b
+Apache HTTP Server: \bApache\sHTTP\sServer\b
+Apache Tomcat: \bApache\sTomcat\b
+Asana: \bAsana\b
+Atom: \bAtom\b
+Babel: \bBabel\b
+Backbone.js: \bBackbone.js\b
+Bitbucket: \bBitbucket\b
+BootStrap: \bBootStrap\b
+Bower: \bBower\b
+C#: \bC#\b
+CSS3: \bCSS\s3\b
+Cassandra: \bCassandra\b
+CentOS: \bCentOS\b
+CircleCI: \bCircleCI\b
+Chart.JS: \bChart.JS\b
+CloudFlare: \bCloudFlare\b
+Confluence: \bConfluence\b
+D3: \bD3\b
+Datadog: \bDatadog\b
+Debian: \bDebian\b
+DigitalOcean: \bDigitalOcean\b
+Django: \bDjango\b
+Docker: \bDocker\b
+Docker Compose: \bDocker\sCompose\b
+ES6: \bES6\b
+ESLint: \bESLint\b
+Elasticsearch: \bElasticsearch\b
+Electron: \bElectron\b
+ExpressJS: \bExpressJS?\b
+Firebase: \bFirebase\b
+Flask: \bFlask\b
+Flutter: \bFlutter\b
+FullPageJS: \bFullPageJS?\b
+G Suite: \bG\sSuite\b
+Git: \bGit\b
+GitHub: \bGitHub\b
+GitHub Pages: \bGitHub\sPages\b
+GitLab: \bGitLab\b
+Glimmer: \bGlimmer\b
+Google AdSense: \bGoogle\sAdSense\b
+Google Analytics: \bGoogle\sAnalytics\b
+Google App Engine: \bGoogle\sApp\sEngine\b
+Google Cloud Platform: \bGoogle\sCloud\sPlatform\b
+Google Compute Engine: \bGoogle\sCompute\sEngine\b
+Google Tag Manager: \bGoogle\sTag\sManager\b
+Gradle: \bGradle\b
+Grafana: \bGrafana\b
+GrapQL: \bGraphQL\b
+Groovy: r'\bGroovy\b'
+Grunt: \bGrunt\b
+HTML5: \bHTML5\b
+Hadoop: \bHadoop\b
+Handlebars: \bHandlebars.js\b
+Heroku: \bHeroku\b
+HubSpot: \bHubSpot\b
+InVision: \bInVision\b
+IntelliJ IDEA: \bIntelliJ\sIDEA\b
+Intercom: \bIntercom\b
+Ionic: \bIonic\b
+Java: \bJava\b
+JavaScript: \bJavaScript\b
+Jenkins: \bJenkins\b
+Jira: \bJira\b
+Kafka: \bKafka\b
+Kibana: \bKibana\b
+Kotlin: \bKotlin\b
+Kebernetes: \bKubernetes\b
+Laravel: \bLaravel\b
+Leaflet: \bLeaflet\b
+Lodash: \bLodash\b
+Logstash: \bLogstash\b
+Mailgun: \bMailgun\b
+MariaDB: \bMariaDB\b
+Markdown: \bMarkdown\b
+Matlab: \bMatLab\b
+Material Design for Angular: \bMaterial\sDesign\sfor\sAngular\b
+Memchached: \bMemcached\b
+Microsoft Azure: \bMicrosoft\sAzure\b
+Microsoft IIS: \bMicrosoft\sIIS\b
+Microsoft SQL Server: \bMicrosoft\sSQL\sServer\b
+Mixpanel: \bMixpanel\b
+Modernizr: \bModernizr\b
+MongoDB: \bMongoDB\b
+MySQL: \bMySQL\b
+NGINX: \bNGINX\b
+New Relic: \bNew\sRelic\b
+Next.JS: \bNext.JS\b
+NoSQL: '\bNoSQL\b
+Node.js: \bNode\.js\b
+Notepad++: \bNotepad\+\+\b
+Objective-C: \bObjective-C\b
+OpenSSL: \bOpenSSL\b
+Oracle: \bOracle\b
+PHP: \bPHP\b
+PayPal: \bPayPal\b
+Perl: \bPerl\b
+PhpStorm: \bPhpStorm\b
+PostgreSQL: \bPostgreSQL\b
+Postman: \bPostman\b
+Polymer: \bPolymer\b
+PyCharm: \bPyCharm\b
+Python: \bPython\b
+RabbitMQ: \bRabbitMQ\b
+Rails: \bRails\b
+React: \bReact\b
+React Native: \bReact\sNative\b
+Redis: \bRedis\b
+Redux: \bRedux\b
+RequireJS: \bRequireJS\b
+Ruby: \bRuby\b
+SQL: \bSQL\b
+SQLite: \bSQLite\b
+Sass: \bSass\b
+Scala: \bScala\b
+Selenium: \bSelenium\b
+Sentry: \bSentry\b
+Shopify: \bShopify\b
+Skype: \bSkype\b
+Slack: \bSlack\b
+Slick: \bSlick\b
+Socket.IO: \bSocket\.IO\b
+SourceTree: \bSourceTree\b
+Spring Boot: \bSpring\sBoot\b
+Stack Overflow: \bStack\sOverflow\b
+Stripe: \bStripe\b
+Sublime Text: \bSublime\sText\b
+Swift: \bSwift\b
+Symfony: \bSymfony\b
+TensorFlow: \bTensorFlow\b
+Terraform: \bTerraform\b
+Travis CI: \bTravis\sCI\b
+Trello: \bTrello\b
+Twilio: \bTwilio\b
+Twilio SendGrid: \bTwilio\sSendGrid\b
+TypeScript: \bTypeScript\b
+Ubuntu: \bUbuntu\b
+Vagrant: \bVagrant\b
+Varnish: \bVarnish\b
+Vim: \bVim\b
+VirtualBox: \bVirtualBox\b
+Visual Studio: \bVisual\sStudio\b
+Visual Studio Code: \bVisual\sStudio\sCode\b
+Vue.js: \bVue\.js\b
+Webpack: \bWebpack\b
+WebStorm: \bWebStorm\b
+WooCommerce: \bWooCommerce\b
+WordPress: \bWordPress\b
+Xamarin: \bXamarin\b
+Xcode: \bXcode\b
+Yarn: \bYarn\b
+Zendesk: \bZendesk\b
+gulp: \bgulp\b
+jQuery: \bjQuery\b
+jQuery UI: \bjQuery UI\b
+npm: \bnpm\b
